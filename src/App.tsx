1d1621e14d7828a5c37bc359660f0843fcc8714e
ymmy <chem-pion@mail.ru>
2025-01-23 03:30:31 -0800

    gh pages tweak

--- src/App.tsx
+++ src/App.tsx
@@ -13,40 +13,20 @@ const App: React.FC = () => {
   const isAuthenticated = false; // Replace this with real auth logic
 
   return (
-    <Router>
+    <Router basename='/fetch-dog-shelter-test'>
       <Routes>
-        <Route path='/fetch-dog-shelter-test/login' element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route
-          path='/fetch-dog-shelter-test'
-          element={
-            isAuthenticated ? (
-              <Home />
-            ) : (
-              <Navigate to='/fetch-dog-shelter-test/login' />
-            )
-          }
+          path='/'
+          element={isAuthenticated ? <Home /> : <Navigate to='/login' />}
         />
         <Route
-          path='/fetch-dog-shelter-test/favorites'
-          element={
-            isAuthenticated ? (
-              <Favorites />
-            ) : (
-              <Navigate to='/fetch-dog-shelter-test/login' />
-            )
-          }
+          path='/favorites'
+          element={isAuthenticated ? <Favorites /> : <Navigate to='/login' />}
         />
         <Route
-          path='/fetch-dog-shelter-test/*'
-          element={
-            <Navigate
-              to={
-                isAuthenticated
-                  ? '/fetch-dog-shelter-test/'
-                  : '/fetch-dog-shelter-test/login'
-              }
-            />
-          }
+          path='*'
+          element={<Navigate to={isAuthenticated ? '/' : '/login'} />}
         />
       </Routes>
     </Router>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'fetch-dog-shelter-test',
+  base: '/fetch-dog-shelter-test/',
   plugins: [react()],
 });
